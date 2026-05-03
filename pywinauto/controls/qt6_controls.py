0f2c2cf7b21b4d6c59701ffb90fee1e980bf81c6
Makarov-AA <alexandr.makarov.nn@gmail.com>
2026-05-03 17:47:13 +0300

    Add Qt6 backend
    Signed-off-by: Makarov-AA <alexandr.makarov.nn@gmail.com>

--- pywinauto/controls/qt6_controls.py
+++ pywinauto/controls/qt6_controls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-"""Wrap various Qt controls. To be used with the 'qt5' backend."""
+"""Wrap various Qt controls. To be used with the 'qt6' backend."""
 
 from __future__ import unicode_literals
 
-from .qt5wrapper import Qt5Wrapper
+from .qt6wrapper import Qt6Wrapper
 from .qt_common_controls import (
     CommonButtonWrapper,
     CommonComboBoxWrapper,
@@ -18,69 +18,70 @@ from .qt_common_controls import (
 )
 
 
-class WindowWrapper(CommonWindowWrapper, Qt5Wrapper):
+class WindowWrapper(CommonWindowWrapper, Qt6Wrapper):
 
     """Wrap a Qt top-level window."""
 
     _control_types = ['Window']
 
 
-class PaneWrapper(CommonPaneWrapper, Qt5Wrapper):
+class PaneWrapper(CommonPaneWrapper, Qt6Wrapper):
 
     """Wrap generic Qt container controls."""
 
     _control_types = ['Pane', 'GroupBox']
 
 
-class ButtonWrapper(CommonButtonWrapper, Qt5Wrapper):
+class ButtonWrapper(CommonButtonWrapper, Qt6Wrapper):
 
     """Wrap Qt button, checkbox, and radio button controls."""
 
     _control_types = ['Button', 'CheckBox', 'RadioButton']
 
 
-class EditWrapper(CommonEditWrapper, Qt5Wrapper):
+class EditWrapper(CommonEditWrapper, Qt6Wrapper):
 
     """Wrap a Qt edit control."""
 
     _control_types = ['Edit']
 
-class ComboBoxWrapper(CommonComboBoxWrapper, Qt5Wrapper):
+
+class ComboBoxWrapper(CommonComboBoxWrapper, Qt6Wrapper):
 
     """Wrap a Qt combobox."""
 
     _control_types = ['ComboBox']
 
 
-class TabControlWrapper(CommonTabControlWrapper, Qt5Wrapper):
+class TabControlWrapper(CommonTabControlWrapper, Qt6Wrapper):
 
     """Wrap Qt tab controls."""
 
     _control_types = ['TabControl']
 
 
-class SliderWrapper(CommonSliderWrapper, Qt5Wrapper):
+class SliderWrapper(CommonSliderWrapper, Qt6Wrapper):
 
     """Wrap Qt slider, scrollbar, spinbox, and progress controls."""
 
     _control_types = ['Slider', 'ScrollBar', 'Spinner', 'ProgressBar']
 
 
-class ListViewWrapper(CommonListViewWrapper, Qt5Wrapper):
+class ListViewWrapper(CommonListViewWrapper, Qt6Wrapper):
 
     """Wrap Qt list controls."""
 
     _control_types = ['List']
 
 
-class TreeViewWrapper(CommonTreeViewWrapper, Qt5Wrapper):
+class TreeViewWrapper(CommonTreeViewWrapper, Qt6Wrapper):
 
     """Wrap Qt tree controls."""
 
     _control_types = ['Tree']
 
 
-class TableWrapper(CommonTableWrapper, Qt5Wrapper):
+class TableWrapper(CommonTableWrapper, Qt6Wrapper):
 
     """Wrap Qt table controls."""
 
